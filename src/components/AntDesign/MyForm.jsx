a48c77966c90fc30390291ae96522a2bedb4f1a8
Nihad <nihadishi@gmail.com>
2023-05-17 11:16:23 +0400

    form

--- src/components/AntDesign/MyForm.jsx
+++ src/components/AntDesign/MyForm.jsx
@@ -1,13 +1,52 @@
 import { Button, Form, Input } from 'antd'
-import { Formik } from 'formik'
+import { Field, Formik } from 'formik'
 import React from 'react'
+import * as Yup from 'yup'
 
 const MyForm = () => {
-  return (
-    <>
-      {/* <Formik */}
-    </>
-  )
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .max(50, 'Max 50 character must be!')
+      .required('Bos olmaz'),
+    lastName: Yup.string()
+    .max(50, 'Max 50 character must be!')
+    .required('Bos olmaz'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
+  return(<>
+    <div>
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={values => {console.log(values)}}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            
+            Name:<Field name="firstName" />
+            {errors.firstName && touched.firstName ? (          
+              <div>{errors.firstName}</div>
+            ) : null}
+
+            Surname:<Field name="lastName" />
+            {errors.lastName && touched.lastName ? (
+              <div>{errors.lastName}</div>
+            ) : null}
+
+            Email:<Field name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+    </>)
 }
 
 export default MyForm
